bd54194df77cf9c58b70ef74a0ad797f578663ca
kaellucaspincordeiro <kaellucaspincord@gmail.com>
2026-05-28 16:27:06 -0300

    Montando a tela do cliente para atender as funções

--- Cliente/tela_cliente.py
+++ Cliente/tela_cliente.py
@@ -30,35 +30,48 @@ def montar_tela_cliente(container, funcao_voltar):
     tk.Label(frame_cliente, text="CPF:", font=("Arial", 10, "bold")).grid(row=7, column=0, pady=(0, 5))
     ent_cpf = tk.Entry(frame_cliente, width=40)
     ent_cpf.grid(row=8, column=0, pady=(0, 10))
+    tk.Label(frame_cliente, text="Status:", font=("Arial", 10, "bold")).grid(row=10, column=0, pady=(0, 2))
+
+    combo_status = ttk.Combobox(
+    frame_cliente,
+    values=["Aluno", "Professor"],
+    width=37,
+    state="readonly"
+    )
+    combo_status.grid(row=11, column=0, pady=(0, 5))
+    
 
     def salvar_cliente():
         nome_cliente = ent_nome.get()
         telefone = ent_telefone.get()
         endereco = ent_endereco.get()
         cpf = ent_cpf.get()
+        status = combo_status.get()
 
         condicao_cliente = (nome_cliente.strip() 
                             and telefone.strip() 
                             and endereco.strip() 
                             and cpf.strip()
+                            and status.strip()
                            )
         
 
         if condicao_cliente:
-            bd.db_cadastrar_cliente(nome_cliente, telefone, endereco, cpf)
+            bd.db_cadastrar_cliente(nome_cliente, telefone, endereco, cpf, status)
             messagebox.showinfo("Sucesso", "Cliente cadastrado com sucesso!")
             ent_nome.delete(0, tk.END)
             ent_telefone.delete(0, tk.END)
             ent_endereco.delete(0, tk.END)
             ent_cpf.delete(0, tk.END)
+            combo_status.set("")
             atualizar_lista()
         else:
             messagebox.showwarning("Aviso", "Preencha todos os campos!")
 
-    tk.Button(frame_cliente, text="Cadastrar cliente", command=salvar_cliente, bg="green", fg="white").grid(row=9, column=0, pady=(0, 15))
+    tk.Button(frame_cliente, text="Cadastrar cliente", command=salvar_cliente, bg="green", fg="white").grid(row=12, column=0, pady=(0, 15))
 
     # --- Lista de Livros ---
-    tk.Label(frame_cliente, text="Clientes Cadastrados:", font=("Arial", 10, "bold")).grid(row=10, column=0, pady=(0, 10))
+    tk.Label(frame_cliente, text="Clientes Cadastrados:", font=("Arial", 10, "bold")).grid(row=13, column=0, pady=(0, 10))
     
     frame_tabela = tk.Frame(container)
     frame_tabela.grid(row=5, column=0, pady=10, padx=20, sticky="nsew")
@@ -71,7 +84,7 @@ def montar_tela_cliente(container, funcao_voltar):
 
     tabela = ttk.Treeview(
         frame_tabela,
-        columns=("id", "cliente", "telefone", "endereço", "cpf"),
+        columns=("id", "cliente", "telefone", "endereço", "cpf", "status"),
         show="headings",
         yscrollcommand=scroll.set
     )
@@ -87,12 +100,14 @@ def montar_tela_cliente(container, funcao_voltar):
     tabela.heading("telefone", text="Telefone")
     tabela.heading("endereço", text="Endereço")
     tabela.heading("cpf", text="CPF")
+    tabela.heading("status", text="Status")
 
     tabela.column("id", width=30, anchor="center")
     tabela.column("cliente", width=30, anchor="center")
     tabela.column("telefone", width=30, anchor="center")
     tabela.column("endereço", width=30, anchor="center")
     tabela.column("cpf", width=30, anchor="center")
+    tabela.column("status", width=30, anchor="center")
 
     frame_botoes = tk.Frame(container)
     frame_botoes.grid(row=7, column=0, pady=10)
@@ -106,7 +121,7 @@ def montar_tela_cliente(container, funcao_voltar):
         clientes = bd.db_listar_clientes()
 
         for cli in clientes:           
-            tabela.insert("", "end", values=(cli[0],cli[1],cli[2],cli[3],cli[4]))
+            tabela.insert("", "end", values=(cli[0],cli[1],cli[2],cli[3],cli[4],cli[5]))
 
     atualizar_lista()
         
@@ -140,6 +155,7 @@ def montar_tela_cliente(container, funcao_voltar):
         telefone = valores[2]
         endereco = valores[3]
         cpf = valores[4]
+        status = valores[5]
 
         janela_cliente = tk.Toplevel(container)
         janela_cliente.title("Atualizar Cliente")
@@ -168,25 +184,38 @@ def montar_tela_cliente(container, funcao_voltar):
         ent_cpf.grid(row=7, column=0, pady=5)
         ent_cpf.insert(0, cpf)
 
+        tk.Label(janela_cliente, text="Status:").grid(row=8, column=0, pady=5)
+        combo_status = ttk.Combobox(
+        janela_cliente,
+        values=["Aluno","Professor"],
+        width=37,
+        state="readonly"
+        )
+        combo_status.grid(row=9, column=0, pady=5)
+        combo_status.set(status)
+
+
         def salvar_atualizacao():
                  
                 nome = ent_nome.get().strip()
                 telefone = ent_telefone.get().strip()
                 endereco = ent_endereco.get().strip()
                 cpf = ent_cpf.get().strip()
+                status = combo_status.get().strip()
 
                 condicao_cliente = (nome == ""
                                     and telefone == ""
                                     and endereco == ""
                                     and cpf == ""
+                                    and status == ""
                                     )
 
                 if condicao_cliente:
                     messagebox.showwarning("Aviso", "Preencha todos os campos!")
                     return
 
-                bd.db_atualizar_cliente(id_cli, nome, telefone, endereco, cpf)
-                messagebox.showinfo("Sucesso", "Categoria atualizada com sucesso!")
+                bd.db_atualizar_cliente(id_cli, nome, telefone, endereco, cpf, status)
+                messagebox.showinfo("Sucesso", "Cliente atualizada com sucesso!")
 
                 janela_cliente.destroy()
                 atualizar_lista()
@@ -197,7 +226,7 @@ def montar_tela_cliente(container, funcao_voltar):
             command=salvar_atualizacao,
             bg="blue",
             fg="white"
-        ).grid(row=8, column=0, pady=20)
+        ).grid(row=10, column=0, pady=20)
 
     atualizar_lista()
 
